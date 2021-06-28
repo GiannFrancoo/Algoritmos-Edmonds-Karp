9928d337f40853e6cf1cfd7ff32d0d39d706ae44
GiannFrancoo <Bentivegnagianfranco@gmail.com>
2021-06-27 22:53:40 -0300

    Update main.kt

--- Edmonds-Karp/src/main.kt
+++ Edmonds-Karp/src/main.kt
@@ -2,6 +2,16 @@ import com.sun.corba.se.impl.orbutil.graph.Graph
 import java.io.*
 import java.util.*
 
+internal class Nodo(
+    var id : Int
+)
+
+internal class Arco(
+    var peso : Int,
+    var origen : Nodo,
+    var destino : Nodo,
+)
+
 /*
 PROCEDURE FordFulkerson (G,s,t,c)
 
@@ -24,9 +34,7 @@ RETURN f
 
 
 fun main(args: Array<String>) {
-
-    
-
+    val G : Pair<List<Nodo>,List<Arco>>
 
 }
 
@@ -38,16 +46,31 @@ fun main(args: Array<String>) {
         c: --
     output
  */
-fun FordFulkerson() : Int{
+private fun FordFulkerson(g: Pair<List<Nodo>,List<Arco>>, s: Nodo, t: Nodo, c:Int) : Int{
+
+    for (i in 0..g.second.size){
+        g.second[i].peso = 0
+    }
+
+    while(true){
+
+    }
+
+    /*
+    FOR cada arco (u,v) en A
+        f[u,v]::=0; f[v,u]::=0
+    ENDFOR
+
+    WHILE existe un camino de
+        aumento p en Gf
+        cf(p)::= capacidad mínima de p
+        FOR cada arco (u,v) en p
+            f[u,v]::=f[u,v]+cf(p)
+            f[v,u]::=-f[u,v]
+        ENDFOR
+    ENDWHILE;
+    */
 
 
     return 1
-}
-
-internal class nodo{
-
-}
-
-internal class arco{
-
 }
